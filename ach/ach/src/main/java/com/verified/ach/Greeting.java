cbf4320b7e396cb0984d0cd079e668d7fea62179
votedoff4 <56640417+votedoff4@users.noreply.github.com>
2020-02-14 12:27:27 -0600

    Add files via upload

---
+++ ach/ach/src/main/java/com/verified/ach/Greeting.java
@@ -0,0 +1,16 @@
+package com.verified.ach;
+
+public class Greeting {
+    private String message;
+    public Greeting(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMesssage(String message) {
+        this.message = message;
+    }
+}
