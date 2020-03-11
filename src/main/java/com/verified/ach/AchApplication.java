ce6c08760133ecea700f7422d55d733a58772988
Joshua Brinkman <43769953+AbsoluteDragoon@users.noreply.github.com>
2020-03-11 10:46:16 -0500

    Add files via upload

---
+++ src/main/java/com/verified/ach/AchApplication.java
@@ -0,0 +1,12 @@
+package com.verified.ach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchApplication.class, args);
+	}
+}
