2921c1a19cacafbf589df58914cea12494612bc3
Joshua Brinkman <43769953+AbsoluteDragoon@users.noreply.github.com>
2020-03-11 11:15:04 -0500

    Delete WebConfig.java

--- src/main/java/com/verified/ach/WebConfig.java
+++
@@ -1,9 +0,0 @@
-package com.verified.ach;// The main purpose of @Configuration classes is to be sources of
-// bean definitions for the Spring IoC Container.
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class WebConfig {
-
-}
